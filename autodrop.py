d08d77329c4f95990238c57487629e6c5793803d
tgbubbles <superchris1001@gmail.com>
2025-03-18 00:32:11 -0700

    better

--- autodrop.py
+++ autodrop.py
@@ -30,7 +30,7 @@ def save_config():
 
 # Optional - Pull files from phone using ADB
 def pull_from_phone():
-    phone_path = "/sdcard/DCIM/Camera"
+    phone_path = "/sdcard/Download/Meta View/"
     local_temp_dir = os.path.join(CONFIG_DIR, 'PhoneDump')
     os.makedirs(local_temp_dir, exist_ok=True)
 
